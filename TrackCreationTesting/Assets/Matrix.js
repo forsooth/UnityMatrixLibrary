6b5cd064df2a9e5e6f3fb683b2779368433f7e6a
michael121yang <michael.yang@tufts.edu>
2015-11-20 03:17:43 -0500

    Added Inverse, GetRow, and Switched Folders
    Inverse still needs an "invertible" check and some things for
    reformating matrices.
    
    Btw, matt, check if mutliplication is working?  Multiplying by inverse
    doesn't get the identity

---
+++ TrackCreationTesting/Assets/Matrix.js
@@ -0,0 +1,310 @@
+#pragma strict
+
+// TODO
+// Make inverse, determinant, equals, and transpose work
+// Change from using javascript arrays to using Unity native double[][] arrays
+// change all data values from int to double, or even better to a void* ambiguous type equivalent
+// debug extensively
+// comment code
+// optimize
+
+public class Matrix {
+
+        var cols : int;
+        var rows : int;
+        var data : Array;
+
+        public function Matrix(cols : int, rows : int) {
+
+                this.cols = cols;
+                this.rows = rows;
+                if (cols == 0 || rows == 0) {
+                        this.data = null;
+                        return;
+                }
+
+                this.data = new Array(cols);
+
+                for (var i = 0; i < cols; i++) {
+                         this.data[i] = new Array(rows);
+                }
+        }
+        
+        public function isSquare() {
+                return this.cols == this.rows;
+        }
+
+        public function numCols() {
+                return this.cols;
+        }
+
+        public function numRows() {
+                return this.rows;
+        }
+
+        public function getAt(col : int, row : int) {
+                var v : int = (this.data[col] as Array)[row];
+                return v;
+        }
+
+        public function setAt(col : int, row : int, value) {
+                (this.data[col] as Array)[row] = value;
+        }
+
+
+        public function copy() {
+                var matrix : Matrix;
+                matrix = new Matrix(this.cols, this.rows);
+                for (var i = 0; i < this.cols; i++) {
+                        for (var j = 0; j < this.rows; j++) {
+                                matrix.setAt(i, j, (this.data[i] as Array)[j]);
+                        }
+                }
+
+                return matrix;
+        }
+
+        public function print() {
+                var line = "";
+                for (var i = 0; i < this.rows; i++) {
+                        for (var j = 0; j < this.cols; j++) {
+                                line += "[" + (this.data[j] as Array)[i] + "]";
+                        }
+                        Debug.Log(line);
+                        line = "";
+                }
+        }
+
+        // Identity
+        public function I(dim : int) {
+                var identity : Matrix;
+                identity = new Matrix(dim, dim);
+
+                for (var i = 0; i < dim; i++) {
+                        for (var j = 0; j < dim; j++) {
+                                if (i != j) {
+                                        identity.setAt(i, j, 0);
+                                } else {
+                                        identity.setAt(i, j, 1);
+                                }
+                        }
+                }
+
+                return identity;
+        }
+
+        public function canMultiply(matrix : Matrix) {
+                return this.cols == matrix.numRows();
+        }
+
+        public function canAdd(matrix : Matrix) {
+                return (this.cols == matrix.numCols()) && (this.rows == matrix.numRows());
+        }
+        
+        public function getRow(i : int) {
+                var a = new Array(this.cols);
+                for (var j = 0; j < this.cols; j++) {
+                        a[j] = (this.data[j] as Array)[i];
+                }
+
+                return a;
+        }
+
+        public function setRow(i : int, row : Array) {
+                for (var j = 0; j < this.cols; j++) {
+                        (this.data[j] as Array)[i] = row[j];
+                }
+        }
+
+        public function getCol(i : int) {
+                return this.data[i];
+        }
+
+        public function setCol(i : int, col : Array) {
+                this.data[i] = col;
+        }
+
+
+        public function add(matrix : Matrix) {
+                if (!canAdd(matrix)) return null;
+
+                var result : Matrix;
+                result = new Matrix(this.cols, this.rows);
+                for (var i = 0; i < this.cols; i++) {
+                        for (var j = 0; j < this.rows; j++) {
+                                var v1 : int = (this.data[i] as Array)[j];
+                                var v2 : int = matrix.getAt(i, j);
+                                var value =  v1 + v2;
+                                result.setAt(i, j, value);
+                        }
+                }
+
+                return result;
+
+        }
+
+        public function sub(matrix : Matrix) {
+                if (!canAdd(matrix)) return null;
+
+                var result : Matrix;
+                result = new Matrix(this.cols, this.rows);
+                for (var i = 0; i < this.cols; i++) {
+                        for (var j = 0; j < this.rows; j++) {
+                                var v1 : int = (this.data[i] as Array)[j];
+                                var v2 : int = matrix.getAt(i, j);
+                                var value =  v1 - v2;
+                                result.setAt(i, j, value);
+                        }
+                }
+
+                return result;
+        }
+
+        private function dotProduct(a1 : Array, a2 : Array) {
+                if (a1.length != a2.length) return null;
+
+                var result = 0;
+
+                for (var i = 0; i < a1.length; i++) {
+                        var v1 : int = a1[i];
+                        var v2 : int = a2[i];
+                        result += v1 * v2;
+                }
+
+                return result;
+        }
+
+        private function minor(matrix : Matrix, col : int) {
+                var result : Matrix;
+                result = new Matrix(matrix.numCols() - 1, matrix.numRows() - 1);
+
+                for (var i = 0; i < matrix.numCols(); i++) {
+                        if (i == col) continue;
+                        for (var j = 1; j < matrix.numRows(); j++) {
+                                if (i < col) {
+                                        result.setAt(i, j - 1, matrix.getAt(i, j));
+                                } else {
+                                        result.setAt(i - 1, j - 1, matrix.getAt(i, j)); 
+                                }
+                        }
+                }
+
+                return result;
+        }
+        
+        public function multiply(matrix : Matrix) {
+                if (this.data == null) return null;
+                if (!canMultiply(matrix)) return null;
+
+                var result : Matrix;
+                result = new Matrix(this.rows, matrix.numCols());
+
+                for (var i = 0; i < this.rows; i++) {
+                        for (var j = 0; j < matrix.numCols(); j++) {
+                                result.setAt(j, i, dotProduct(matrix.getCol(j), this.getRow(i)));                                
+                        }
+                }
+
+                return result;
+          
+        }
+
+
+        public function determinant() {
+                if (!this.isSquare()) return null;
+
+                return 0;
+                
+        }
+        
+        //Needs checking to see if matrix is invertible
+        public function inverse() {
+                if (this.data == null) return null;
+                if (!this.isSquare()) return null;
+            
+                var copy : Matrix;
+                copy = this;
+                var identity : Matrix;
+                identity = I(this.cols);
+            
+	            var numerator : float; 
+	            var denominator : float; 
+	            var element : float;
+	            var element2 : float;                
+	            var upper = new Array(copy.cols);
+	            var upper_i = new Array(copy.cols);
+ 	            var lower = new Array(copy.cols);
+	            var lower_i = new Array(copy.cols);
+           
+                for(var i = 0; i < copy.rows - 1; i++) {
+	                upper = copy.getRow(i);
+	                upper_i = identity.getRow(i);      
+	                denominator = upper[i];
+	                
+                	for(var j = i+1; j < copy.cols; j++) {
+	                    lower = copy.getRow(j);
+	                    lower_i = identity.getRow(j);                   
+                     	numerator = lower[i];
+	                    		                    	                    
+	                    for(var k = 0; k < copy.cols; k++) {
+	                    	element = upper[k];
+	                    	element2 = lower[k];
+	                        lower[k] = element2 - numerator/denominator*element;
+	                    	element = upper_i[k];
+	                    	element2 = lower_i[k];
+	                        lower_i[k] = element2 - numerator/denominator*element;
+	                    }
+	                    
+	                    copy.setRow(j, lower);
+	                    identity.setRow(j, lower_i);
+                    }
+                }
+
+                for(i = copy.rows - 1; i > 0; i--) {                   
+                	lower_i = identity.getRow(i);
+                    lower = copy.getRow(i);
+                   	denominator = lower[i];
+
+                    for(j = i; j > 0; j--) {
+	                    upper = copy.getRow(j-1);
+	                    upper_i = identity.getRow(j-1);
+                     	numerator = upper[i];
+	                    
+	                    for(k = 0; k < copy.cols; k++) {
+	                    	element = lower[k];
+	                    	element2 = upper[k];
+	                        upper[k] = element2 - numerator/denominator*element;
+	                     	element = lower_i[k];
+	                    	element2 = upper_i[k];
+	                        upper_i[k] = element2 - numerator/denominator*element;
+	                    }
+	                    
+	                    copy.setRow(j-1, upper);
+	                    identity.setRow(j-1, upper_i);
+                    }                    
+                }
+                    
+                for(i = 0; i < copy.rows; i++) {
+                    upper = copy.getRow(i);
+                   	denominator = upper[i];
+                    
+                    for (j = 0; j < copy.cols; j++) {
+                    	numerator = (copy.data[j] as Array)[i];
+                        (copy.data[j] as Array)[i] = numerator / denominator;
+                    	numerator = (identity.data[j] as Array)[i];
+                        (identity.data[j] as Array)[i] = numerator / denominator;
+                    }
+                }          
+                
+                return identity;
+        }
+
+
+        public function transpose() {
+                return this;
+        }
+
+        public function equals(matrix : Matrix) {
+                return true;
+        }
+}
---
+++ TrackCreationTesting/Assets/MatrixTesting.js
@@ -0,0 +1,56 @@
+#pragma strict
+
+function Start () {
+        var m : Matrix;
+        m = new Matrix(4, 4);
+        m.setAt(0, 0, 1);       
+        m.setAt(1, 0, 2);      
+        m.setAt(2, 0, 3);        
+        m.setAt(3, 0, 4);
+        m.setAt(0, 1, 1);     
+        m.setAt(1, 1, 5);
+        m.setAt(2, 1, 1);
+        m.setAt(3, 1, 2);
+        m.setAt(0, 2, 0);
+        m.setAt(1, 2, 0);
+        m.setAt(2, 2, 1);
+        m.setAt(3, 2, 1);
+        m.setAt(0, 3, 0);
+        m.setAt(1, 3, 0);
+        m.setAt(2, 3, 0);
+        m.setAt(3, 3, 1);
+
+        m.print();
+
+        Debug.Log("");
+
+        var n : Matrix = m.I(4);
+        n.print();
+
+        Debug.Log("");
+
+        var o : Matrix = m.multiply(n);
+        o.print();
+        Debug.Log("");
+
+        var p : Matrix;
+        p = n.add(n);
+        //p.print();
+        Debug.Log("");
+
+        Debug.Log(m.determinant());
+    
+        var q : Matrix;
+        q = m.inverse();
+        q.print();
+        q = q.inverse();
+        q.print();
+        Debug.Log("");
+		
+		q = q.multiply(q.inverse());
+		q.print();
+}
+
+function Update () {
+
+}
